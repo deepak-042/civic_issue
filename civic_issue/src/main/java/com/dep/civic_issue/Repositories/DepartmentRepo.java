4eb51ecef468d9bf75360071d6c2ce945b521275
deepak-042 <deepaknote548@gmail.com>
2025-11-10 10:50:06 +0530

    departmentRepo changed

--- civic_issue/src/main/java/com/dep/civic_issue/Repositories/DepartmentRepo.java
+++ civic_issue/src/main/java/com/dep/civic_issue/Repositories/DepartmentRepo.java
@@ -4,4 +4,5 @@ import com.dep.civic_issue.Entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepo extends JpaRepository<Department,Long> {
+
 }
